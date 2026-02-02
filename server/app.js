e972077ef71d1612d6771326a47b29fe405f4749
荼白 <2728347417@qq.com>
2026-02-03 04:27:07 +0800

    feat: 更新 .gitignore，添加环境变量示例和启动文件，配置基础依赖，跑通后端最小化启动代码 fix：修复github actions的报错

--- server/app.js
+++ server/app.js
@@ -1,2 +1,19 @@
 ﻿// [入口] 启动文件，配置中间件、静态资源、错误处理
-// 待实现。
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('EasyStay Backend is Running on Port 3000!');
+});
+
+// 测试数据库连接 (可选，暂时先不连，保证先跑起来)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
