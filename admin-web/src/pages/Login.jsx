a010dad3c1d21e49bfffafa3831ffb6999f6784d
Azure-wang <3088912833@qq.com>
2026-02-03 11:56:59 +0800

    feat: 后台登录 UI 页面初始实现

--- admin-web/src/pages/Login.jsx
+++ admin-web/src/pages/Login.jsx
@@ -1,2 +1,23 @@
 ﻿// [页面] 登录页
-// 待实现。
+import React from 'react'
+import { Card, Form, Input, Button } from 'antd'
+
+const Login = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Card title="EasyStay 管理后台登录" style={{ width: 400 }}>
+        <Form>
+          <Form.Item label="用户名">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码">
+            <Input.Password />
+          </Form.Item>
+          <Button type="primary" block>登录</Button>
+        </Form>
+      </Card>
+    </div>
+  )
+}
+
+export default Login
