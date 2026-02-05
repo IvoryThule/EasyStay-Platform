f41b26b0bbdef59554820bec10224d61673ccfc2
sytttt12345 <1003437192@qq.com>
2026-02-05 18:18:07 +0800

    feat: 酒店查询页面；加入了日历组件

--- mobile-app/src/app.config.js
+++ mobile-app/src/app.config.js
@@ -11,5 +11,26 @@
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'EasyStay',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+      custom: true,
+      color: '#666',
+      selectedColor: '#ed6c00',
+      backgroundColor: '#fafafa',
+      borderStyle: 'black',
+      
+      list: [{
+        pagePath: 'pages/index/index',
+        text: '首页查询'
+      }, {
+        pagePath: 'pages/list/index',
+        text: '目的地'
+      }, {
+        pagePath: 'pages/detail/index',
+        text: '订单'
+      },{
+        pagePath: 'pages/user/index',
+        text: '我的'
+      }]
   }
 })
